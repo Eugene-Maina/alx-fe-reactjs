8558edb0dc1361e34fc964730a2f537d15fd823f
Eugene-Maina <mainaeugene23@gmail.com>
2025-09-07 00:59:34 +0300

    added the use state hook

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './Header'
 import MainContent from './components/MainContent'
 import Footer from './Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +18,7 @@ function App() {
        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Header />
       <MainContent />
+      <Counter />
       <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
