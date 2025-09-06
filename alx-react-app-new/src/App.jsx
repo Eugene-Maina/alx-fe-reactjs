c7bfae7ad66f1a22b4230f69fa35d93de579a38c
Eugene-Maina <mainaeugene23@gmail.com>
2025-09-07 00:48:09 +0300

    added style to main content

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,7 +4,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Header'
-import MainContent from './MainContent'
+import MainContent from './components/MainContent'
 import Footer from './Footer'
 import UserProfile from './components/UserProfile'
 
