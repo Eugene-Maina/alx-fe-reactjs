3be921a5ac5c66f39f8eb26ad3a8f8114716c47e
Eugene-Maina <mainaeugene23@gmail.com>
2025-09-19 11:51:20 +0300

    Added the recipe sharing app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <RecipeList/>
+      <AddRecipeForm/>
+      <div>
+        <h1>Recipe Sharing App</h1>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
